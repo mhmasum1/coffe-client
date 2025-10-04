2c194aa8f231c3fe554576e4c94ec3fde0c5520e
mhmasum1 <mhmasum231@gmail.com>
2025-10-04 17:47:17 +0600

    added send to coffe data to mongodb

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,29 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Mainlayouts from './layouts/Mainlayouts.jsx';
+import Home from './components/Home.jsx';
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Mainlayouts />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'addcoffee',
+        element: <AddCoffee />
+      },
+      {
+        path: 'updateCoffee',
+        element: <UpdateCoffee />
+      }
+    ]
   },
 ]);
 
