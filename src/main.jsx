43b4b69bd2941863656bc8883f41f86451a89607
mhmasum1 <mhmasum231@gmail.com>
2025-10-05 11:08:47 +0600

    added updateCoffee components and update from database

--- src/main.jsx
+++ src/main.jsx
@@ -31,6 +31,7 @@ const router = createBrowserRouter([
       },
       {
         path: 'updateCoffee/:id',
+        loader: ({ params }) => fetch(`http://localhost:3000/coffees/${params.id}`),
         element: <UpdateCoffee />
       }
     ]
